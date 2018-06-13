7f7ffc03914080641d0e988fdb9177d03e39cc13
Florian Zenoni <florianzenoni@gmail.com>
2018-06-13 15:49:41 +0200

    Remove useless piece of code

--- analysis.R
+++
@@ -1,9 +0,0 @@
-library(data.table)
-
-data <- fread('../osrm-application/output.csv')
-
-# Compute average duration for 5 closer destinations
-avg <- data[, .(avg = mean(head(sort(V2), 5))), by = V1]
-# avg <- avg5[, .(avg = mean(closer5)), by = V1]
-setnames(avg, 'V1', 'id')
-avg[, avg := avg/60]
